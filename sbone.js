2e52cce6feb4358b8e4d9a3acbeb581e0082aaf3
Agrim1234 <agrimagarwal123456789@gmail.com>
2018-06-12 16:03:48 +0530

    display improvements

--- sbone.js
+++ sbone.js
@@ -147,7 +147,26 @@ $(document).ready(function(){
     $("#drop4").click(function(){
       window.location.href = "aboutus.html"
     });
+
 });
+
+var count_particles, stats, update;
+stats = new Stats;
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector('.js-count-particles');
+update = function() {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
 
 $(document).ready(function(){
   $('#fullpage').fullpage({
