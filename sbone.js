72e2871a94f0cc21ecb5e4bda46d1205aa6c2140
2017A7PS0927G <f20170927@goa.bits-pilani.ac.in>
2018-09-02 12:31:40 +0530

    done

--- sbone.js
+++ sbone.js
@@ -432,6 +432,26 @@ window.onclick = function(event) {
     }
 } 
 
+/*var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot1");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
+}*/
+
 var count_particles, stats, update;
 stats = new Stats;
 stats.setMode(0);
